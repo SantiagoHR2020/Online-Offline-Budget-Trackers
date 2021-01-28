e325e0862c8a5dcd4cace6f4aebfbc99aa6c03d5
Santiago Hincapie <santiagohr81@hotmail.com>
2021-01-28 18:10:32 -0500

    server-worker DATA_CACHE_NAME updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -97,7 +97,7 @@ self.addEventListener("fetch", event => {
   if (event.request.url.includes("/api/transaction")) {
     // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.open(RUNTIME_CACHE).then(cache => {
+      caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request)
           .then(response => {
             cache.put(event.request, response.clone());
@@ -117,7 +117,7 @@ self.addEventListener("fetch", event => {
       }
 
       // request is not in cache. make network request and cache the response
-      return caches.open(RUNTIME_CACHE).then(cache => {
+      return caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request).then(response => {
           return cache.put(event.request, response.clone()).then(() => {
             return response;
