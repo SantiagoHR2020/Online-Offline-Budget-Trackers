8de4838975236263ca12bbfd3b498ed975f6c503
Santiago Hincapie <santiagohr81@hotmail.com>
2021-01-30 13:07:36 -0500

    con

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
